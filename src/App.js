29f20beed8ccdb8880995167c50d1c50b6bd9150
Vnovnick <safonov.nick10@gmail.com>
2023-12-29 12:11:40 -0800

    add components and corresponding folders; add meta tags

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Nav from "./components/Nav/Nav";
 
 function App() {
-  return <div className="App">Homepage</div>;
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <ul>
+      <li>
+        <a>Home</a>
+      </li>
+      <li>
+        <a>About</a>
+      </li>
+      <li>
+        <a>Menu</a>
+      </li>
+      <li>
+        <a>Reservations</a>
+      </li>
+      <li>
+        <a>Order Online</a>
+      </li>
+      <li>
+        <a>Login</a>
+      </li>
+    </ul>
+  );
+}
